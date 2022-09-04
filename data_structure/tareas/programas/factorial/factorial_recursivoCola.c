ce3b8d6a7511ed7f7c518fc2b3c3656ea9103e1f
AlaN <digitalforce.ac@gmail.com>
2022-09-03 22:43:41 -0500

    Factorial_recursivoCola añadido

---
+++ data_structure/tareas/programas/factorial/factorial_recursivoCola.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//Complejidad O(n)
+unsigned long long Factorial(int n, int a)
+{
+    if(n == 0 || n == 1)
+    {
+        return a;
+    }
+
+    return Factorial((n-1), a * n);
+
+}
+
+int main(void)
+{
+    int n;
+    int a = 1;
+
+
+    scanf("%d", &n);
+
+    printf("%llu", Factorial(n, a));
+    
+    return 0;
+}
