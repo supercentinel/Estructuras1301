9e5b4deefab49d7e3bb829263e99766e7f7dba79
AlaN <digitalforce.ac@gmail.com>
2022-11-20 23:47:17 -0600

    infixToPosfix() It Works!

--- data_structure/code/main/calculadora/main.c
+++ data_structure/code/main/calculadora/main.c
@@ -167,7 +167,7 @@ int insert(List *infix, char *ifs)
 
 int infixToPosfix(List *infix, List *posfix)
 {
-    int i, j, f, fp, iflen = list_size(infix);
+    int i, f, fp, iflen = list_size(infix);
 
     char *data, *datapf, *dataAux;
 
@@ -178,6 +178,7 @@ int infixToPosfix(List *infix, List *posfix)
 
     node = list_head(infix);
 
+    //itera sobre la lista infija
     for (i = 0; i < iflen; i++)
     {
         data = list_data(node);
@@ -188,19 +189,77 @@ int infixToPosfix(List *infix, List *posfix)
 
         f = filtrar(datapf[0]);
 
-        if(f == 0)
+        //si es un parentesis( se introduce en el stack
+        if(f == 4)
         {
-            if(list_ins_next(posfix, list_tail(posfix), datapf) != 0) return -1;
+            printf("parentesis en(: %d\n", i);
+            if((stack_push(&stack, datapf)) != 0) return -1;
+            node = list_next(node);
+            //reset a la precedencia
+            fp = 0;
+            //printf("fp value = %d", fp);
+            continue;
         }
-        else
+
+        //cuando hay un parentesis) cerrando
+        if(f == 5)
         {
-            
-            if(stack_size(&stack) > 0 && f <= fp)
+            printf("parentesis en): %d\n", i);
+            data = stack_peek(&stack);
+            printf("popping\n");
+            printf("data = %s\n", data);
+
+            //se hace un pop al stack hasta cerrar los parentesis
+            do
             {
-                printf("\n%d\n", stack_size(&stack));
-                
                 if(stack_pop(&stack, (void **)&data) == 0)
                 {
+                    //aqui el parentesis cierra y termina el loop
+                    if(filtrar(data[0]) == 4)
+                    {
+                        free(data);
+                        break;
+                    }
+                    if((datapf = (char *)malloc(sizeof(char) * strlen(data))) == NULL) return -1;
+
+                    strcpy(datapf, data);
+
+                    if(filtrar(datapf[0]) != 4)
+                        if(list_ins_next(posfix, list_tail(posfix), datapf) != 0) return -1;
+
+                    free(data);
+                }
+                else
+                {
+                    return -1;
+                }
+            }while (1);
+            //y se continua con la iteracion
+            node = list_next(node);
+            //printf("fp value = %d\n", fp);
+            //reset a la precedencia
+            fp = 0;
+            continue;
+        }
+
+        //si es un numero se va a la lista
+        if(f == 0)
+        {
+            printf("digito en: %d\n", i);
+            if(list_ins_next(posfix, list_tail(posfix), datapf) != 0) return -1;
+        }
+        //en caso contrario es un operador y va al stack
+        else
+        {
+            printf("operador en: %d\n", i);
+            //printf("fp value = %d\n", fp);
+
+            //si ya hay elementos en el stack se comprueba que la precedencia
+            if(stack_size(&stack) > 0 && f <= fp)
+            {   
+                if(stack_pop(&stack, (void **)&data) == 0)
+                {
+                    printf("pop en: %d\n", i);
                     if((dataAux = (char *)malloc(sizeof(char) * strlen(data))) == NULL) return -1;
 
                     strcpy(dataAux, data);
@@ -214,14 +273,17 @@ int infixToPosfix(List *infix, List *posfix)
                     return -1;
                 }
             }
-
-            if((stack_push(&stack, datapf)) != 0) return -1;
+            //se establece la precedencia para la siguiente iteracion
             fp = f;
+            if((stack_push(&stack, datapf)) != 0) return -1;
+            data = stack_peek(&stack);
+            printf("pushed: %s\n", data);
         }
 
         node = list_next(node);
     }
 
+    //si la iteracion sobre la lista infija termino, pero el stack no esta vacio se hace un pop hasta que lo este
    while (stack_size(&stack) > 0)
    {
         if(stack_pop(&stack, (void **)&data) == 0)
